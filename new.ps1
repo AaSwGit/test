21275d6afb8a9e79cbe14a4681aad1a0c8c59e07
AaSwGit <53932630+AaSwGit@users.noreply.github.com>
2022-04-25 08:25:31 +0300

    Create new.ps1

---
+++ new.ps1
@@ -0,0 +1,14 @@
+new-module -name foobar -scriptblock {
+  Function Install-Project() {
+    param (
+      [string]$project = 'chef',
+      [string]$channel = 'stable'
+    )
+    echo $project $channgel
+  }
+
+  set-alias install -value Install-Project
+
+  export-modulemember -alias 'install'
+  }
+  
